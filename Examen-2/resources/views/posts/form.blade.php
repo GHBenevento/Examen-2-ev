dd5b0b5e80a0982ca7b2e3683a8a29655434a660
Georgie <gbenevento@cifpfbmoll.eu>
2022-02-10 19:30:13 +0100

    2.1 - Added old values to form and errors

--- Examen-2/resources/views/posts/form.blade.php
+++ Examen-2/resources/views/posts/form.blade.php
@@ -1,9 +1,12 @@
 <form class="form text-start bg-light m-5" method="post" enctype="multipart/form-data">
     <div>
-        <div>
+        <p class="text-danger">
+            {{ $error }}
+        </p>
 
+        <div>
             <div class="form-group m-3 p-2">
-                <input class="form-control" type="text" name="title" required>
+                <input class="form-control" type="text" name="title" value="{{ old('title'), $post->title }}" required>
                 @error('title')
                 <br>
                 <p class="text-danger">*{{$message}}</p>
@@ -12,7 +15,7 @@
             </div>
 
             <div class="form-group m-3 p-2">
-                <input class="form-control" type="text" name="extract" required>
+                <input class="form-control" type="text" name="extract" value="{{ old('extract'), $post->extract }}" required>
                 @error('extract')
                 <br>
                 <p class="text-danger">*{{$message}}</p>
@@ -21,7 +24,7 @@
             </div>
 
             <div class="form-group m-3 p-2">
-                <input class="form-control" type="text" name="content" required>
+                <input class="form-control" type="text" name="content" value="{{ old('content'), $post->content }}" required>
                 @error('content')
                 <br>
                 <p class="text-danger">*{{$message}}</p>
@@ -46,7 +49,7 @@
 
             <div class="form-group m-3 p-2">
 
-                <input type="checkbox" name="expire">
+                <input type="checkbox" name="expire" value="{{ old('expire') == 'on' ? 'checked' : '' }}">
 
                 @error('expire')
                 <br>
@@ -56,14 +59,13 @@
             </div>
 
             <div class="form-group m-3 p-2">
-                <input type="checkbox" name="comment">
+                <input type="checkbox" name="comment" value="{{ old('comment') == 'on' ? 'checked' : '' }}">
                 @error('comment')
                 <br>
                 <p class="text-danger">*{{$message}}</p>
                 <br>
                 @enderror
             </div>
-
 
 
             <input type="hidden" name="user_id" id="user_id" class="form-control" value="{{ Auth::user()->id}}">
